de1d2f8c584fddf8061667504d84ae496f8b9356
Oria <oriadam@gmail.com>
2018-10-02 17:20:46 +0300

    Update floating_thead.js
    rewrite the whole thing to avoid clones and simplify usage

--- floating_thead.js
+++ floating_thead.js
@@ -1,72 +1,75 @@
-floating_thead_data=[]
-
-function floating_thead_init() {
-	jQuery('thead').each(function(){
+jQuery.fn.floating_thead = function (options)
+{
+	this.each(function ()
+	{
 		// init
-		var data={}
-		var thead=this
-		var $thead=jQuery(thead)
-		var $table=$thead.closest('table')
-		if (!$table.length)	return
-		if (!$table.scrollParent){
-			console.log('floating_thead requires jQueryUI. Aborting :(')
-			return
-		}
-		var $scrollParentForEvent=$table.scrollParent()
-		var $scrollParent=$scrollParentForEvent
+		var $table = jQuery(this);
+		var $thead = $table.find('thead');
+		var $tbody = $table.find('tbody');
+		if (!($thead.length || $tbody.length)) return;
+		var $scrollParentForEvent = jQuery(options.scrollParent || $table.scrollParent());
+		var $scrollParent = $scrollParentForEvent;
 		if (!$scrollParentForEvent[0].style) // if scrollParent is window.document use <body> instead
-			$scrollParent=jQuery('body')
+			$scrollParent = jQuery('body');
+		var $thead_children = $thead.find('th');
+		var $tbody_children = $tbody.find('tr:first > td');
+		var correct_top;
 
-		// runonce
-		if ($scrollParent.data('floating_thead_runonce'))
-			return
-		$scrollParent.data('floating_thead_runonce',1)
-		
-		// clone thead
-		var $thead_clone=$thead.clone().css({position:'fixed',margin:0}).hide().addClass('floating_thead_pinned').appendTo($table)
-
-		// save in the global object. used by floating_thead_onscroll()
-		data.$thead_clone=$thead_clone
-		data.$thead_children=$thead.find('th,td')
-		data.$thead_clone_children=$thead_clone.find('th,td')
-		data.$thead=$thead
-		data.$table=$table
-		data.$scrollParent=$scrollParent
-		floating_thead_data.push(data)
-
-		// attach event
-		$scrollParentForEvent.on('scroll',function(ev){
-			//console.log('floating_thead attach scroll',data)
-			floating_thead_onscroll(ev,data)
-		})
-	})
-}
-
-function floating_thead_onscroll(ev,data) {
-	var scrollTop = data.$scrollParent.scrollTop()
-	var thead_clone_top = data.$scrollParent.offset().top
-	var floatingIsNeeded=scrollTop > 0
-	floating_thead_last_data=data
-	//console.log('floating_thead_onscroll',floatingIsNeeded,ev,data)
-
-	if (floatingIsNeeded) {
-		data.$thead_clone.css("top",thead_clone_top).show();
-		if (!data.$thead.data('runonce')){
-			data.$thead.data('runonce',1)
-			data.$thead_clone.width(data.$thead.width())
-			data.$thead_clone_children.each(function(i,clone){
-				clone.style.width=data.$thead_children[i].offsetWidth+'px'
-			})
+		function reset()
+		{
+			$tbody.css('transform', '');
+			$thead.css({position: '', top: ''});
+			$table.removeClass('floating_thead_on').data('floating_thead', null);
+			$thead_children.add($tbody_children).css('width', '');
 		}
-		if (data.$scrollParent[0].offsetWidth<data.$scrollParent[0].scrollWidth) // has h-scrolling
-			if (data.$thead_clone.css('marginLeft')!=-data.$scrollParent.scrollLeft())
-				data.$thead_clone.css('marginLeft',-data.$scrollParent.scrollLeft())
-	}else{
-		data.$thead_clone.hide()
-		data.$thead.data('runonce',0)
-	}
-}
 
-// run on load
-jQuery(floating_thead_init);
+		function on_scroll()
+		{
+			var floatingIsNeeded = $table.offset().top < $thead.height();
+			if ($table.data('floating_thead') != floatingIsNeeded)
+			{
+				correct_top = Math.max(0, $table.offset().top || 0);
+				$table.data('floating_thead', floatingIsNeeded);
+				$tbody.css('transform', 'translate(0,' + $thead.height() + 'px)');
+				if (floatingIsNeeded)
+				{
+					$thead_children.each(function (i, th)
+					{
+						var w = Math.max($tbody_children[i].offsetWidth, th.offsetWidth) + 'px';
+						th.style.width = w;
+						$tbody_children[i].style.width = w;
+					});
+					if ($scrollParent[0].offsetWidth < $scrollParent[0].scrollWidth) // has h-scrolling
+						if ($thead.css('marginLeft') != -$scrollParent.scrollLeft())
+							$thead.css('marginLeft', -$scrollParent.scrollLeft());
+					$thead.css({position: 'fixed', top: correct_top});
+				}
+				else
+				{
+					reset();
+				}
+				$table.toggleClass('floating_thead_on', floatingIsNeeded);
+			}
+			options.on_scroll && options.on_scroll(floatingIsNeeded, $table, $thead);
+		}
 
+		reset();
+		jQuery(window).on('resize', function ()
+		{
+			reset();
+			on_scroll();
+		});
+
+		$scrollParentForEvent.on('scroll', function ()
+		{
+			on_scroll();
+		});
+
+		$table.on('floating_thead_refresh', function ()
+		{
+			reset();
+			on_scroll();
+		});
+	});
+	return this;
+};
