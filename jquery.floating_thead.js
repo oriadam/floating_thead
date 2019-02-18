fb935ac00d9df83e282994a6ef29b2378ab16def
Oria <oriadam@gmail.com>
2019-02-18 11:08:21 +0200

    Update jquery.floating_thead.js

--- jquery.floating_thead.js
+++ jquery.floating_thead.js
@@ -1,11 +1,13 @@
-// https://github.com/oriadam/floating_thead v2.1
+// https://github.com/oriadam/floating_thead v2.2
 /**
  * Make thead sticky for a table
  * To trigger recalc call scrollParent.trigger('scroll')
  * @param options object {
+ * 		fixed: true/false: use 'fixed' or 'absolute' ?
  *		scrollParent: element that has the scroll. default = $table.scrollParnet()
- *		floating_on: true/false/function decide when to make the thead fixed
- *		on_scroll: function(floatingIsNeeded, $table, $thead); what to do when scroll is called
+ *		on_scroll: function($table, $thead); what to do after scroll was called
+ *		on_before_scroll: function($table, $thead); what to do before running on_scroll logic
+ *		on_reset: function($table, $thead); what to do after resetting all widths
  * }
  * @returns {jQuery}
  */
@@ -16,7 +18,7 @@ jQuery.fn.floating_thead = function (options)
 		// init
 		if (!options)
 			options = {};
-		var $table = jQuery(this);
+		var $table = jQuery(this).addClass('floating_thead');
 		var $thead, $tbody;
 		var $scrollParentForEvent = options.scrollParent === undefined ? $table.scrollParent() : jQuery(options.scrollParent);
 		var $scrollParent = $scrollParentForEvent;
@@ -29,58 +31,47 @@ jQuery.fn.floating_thead = function (options)
 			$tbody = $table.find('> tbody,> tfoot');
 			$tbody.css('transform', '');
 			$thead.css({position: '', top: '', left: ''});
-			$table.removeClass('floating_thead_on').data('floating_thead_on', null);
-			$table.find('th,td').css('width', '');
-			if (floating_is_needed())
-				$table.find('> thead > tr > th,> tbody > tr:first > td').each(function ()
-				{
-					jQuery(this).outerWidth(this.offsetWidth);
-				});
-		}
-
-		function floating_is_needed()
-		{
-			if (options.floating_on === undefined)
+			$table.css('width', '').find('th,td').css('width', '');
+			$table.offsetTop; // statement to make sure calling page repaint
+			$table.width($table.width() - (options.fixed ? 0  : 0));
+			$thead.width($table.width());
+			$table.find('> thead > tr > th,> tbody > tr:first > td').each(function ()
 			{
-				var top = $scrollParent[0] === document.body ? $table[0].getBoundingClientRect().y : $table.offset().top;
-				return top < $thead.height();
-			}
-			if (typeof options.floating_on === "function")
-				return options.floating_on();
-			return options.floating_on;
+				jQuery(this).outerWidth(this.offsetWidth);
+			});
+			$thead.css({
+				position: options.fixed ? 'fixed' : 'absolute',
+				'z-index': +$thead.css('z-index') || 1
+			});
+			if (options.on_reset) options.on_reset($table, $thead);
+			on_scroll();
 		}
 
 		function on_scroll()
 		{
-			var floatingIsNeeded = floating_is_needed();
-			$table.data('floating_thead_on', floatingIsNeeded);
-			$table.toggleClass('floating_thead_on', floatingIsNeeded);
-			if (floatingIsNeeded)
-			{
-				$tbody.css('transform', 'translate(0,' + $thead.height() + 'px)');
+			options.on_before_scroll && options.on_before_scroll($table, $thead);
+			$tbody.css('transform', 'translate(0,' + $thead.outerHeight(true) + 'px)');
+			if (options.fixed)
 				$thead.css({
-					position: 'fixed',
-					top: Math.max(0, $table.offset().top || 0),
-					left: $table.find('tbody')[0].getBoundingClientRect().left || 0
+					top: Math.max(0, $table[0].getBoundingClientRect().top),
+					left: $table.find('tbody')[0].getBoundingClientRect().left
 				});
+			else
+			{
+				$thead.css({
+					top: Math.max(0, $table[0].offsetTop) + $scrollParent[0].scrollTop,
+					left: $table[0].offsetLeft
+				});
+				if ($scrollParent.css('position') === 'static')
+					$scrollParent.css('position', 'relative');
 			}
-			options.on_scroll && options.on_scroll(floatingIsNeeded, $table, $thead);
+			options.on_scroll && options.on_scroll($table, $thead);
 		}
 
-		jQuery(window).on('resize', function ()
-		{
-			reset();
-			on_scroll();
-		});
-
+		jQuery(window).on('resize', reset);
 		$scrollParentForEvent && $scrollParentForEvent.on('scroll', on_scroll);
-
-		$table.on('floating_thead_refresh', function ()
-		{
-			reset();
-			on_scroll();
-		}).trigger('floating_thead_refresh')
-			.on('floating_thead_scroll', on_scroll);
+		$table.on('floating_thead_refresh', reset).on('floating_thead_scroll', on_scroll);
+		reset();
 	});
 	return this;
 };
